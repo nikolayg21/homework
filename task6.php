a30b71fc152c3498eb9d44a23a7f8466ec52c310
nikolayg21 <nikolayg21@hotmail.com>
2014-12-04 11:31:23 +0200

    hmwrk-php4

---
+++ task6.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RIGHT</title>
+
+	<style type="text/css">
+		body {
+			background-color: #1089E0;
+			padding-left: 1200px;
+			margin: 5px;
+
+		}
+
+		span {
+			color: white;
+			font-weight: bolder;
+		}
+
+		div {
+			color: #666633;
+			border: 5px;
+			border-color: light blue;
+		}
+
+
+	</style>
+
+</head>
+<body>
+<div>
+	<span>Starting off</span>
+		<p>Start here -</p>
+		<p>My first site -</p>
+	</d>
+</div>
+<br>
+
+<div>
+	<span>Lessons</span>
+		<p>Text -</p>
+		<p>Images -</p>
+		<p>Tables -</p>
+		<p>Frames -</p>
+		<p>Forms -</p>
+		<p>Stylesheets -</p>
+</div>
+<br>
+
+<div>
+	<span>Advanced</span>
+		<p>Accessibility -</p>
+		<p>Promotion -</p>
+		<p>Optimisation -</p>
+		<p>Site Management -</p>
+</div>	
+<br>
+
+<div>
+	<span>Scripting</span>
+		<p>Java Script -</p>
+		<p>CGI Scripting -</p>
+</div>
+<br>
+
+<div>
+	<span>Reviews</span>
+		<p>Web Hosting -</p>
+		<p>Books -</p>
+		<p>Recommendations -</p>
+</div>
+</body>
+</html>
